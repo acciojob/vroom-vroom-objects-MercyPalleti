04a9571def54448c3072d992d04b4ae4d2f7649f
MercyPalleti <palletimercy1@gmail.com>
2025-01-25 20:47:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ class Car{
 
 class SportsCar extends Car{
 	constructor(make, model, topSpeed) {
-		this.make=make;
-		this.model=model;
+		super(make,model);
 		this.topSpeed=topSpeed;
 	}
 	getTopSpeed(){
